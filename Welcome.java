59b89d5daa849d2ebd30e43fe08552d1e0dcba79
PauleenSayat <163841476+PauleenSayat@users.noreply.github.com>
2024-08-30 13:56:22 +0800

    Update Welcome.java

--- Welcome.java
+++ Welcome.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Main {
+    
+    public static void main(String[] args){
+        System.out.println("Welcome to CS 211: Object-oriented Programming!");
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = input.nextLine();
+        
+        System.out.println("This course will be fun, " + name + "!");
+        input.close();
+    }
+}
