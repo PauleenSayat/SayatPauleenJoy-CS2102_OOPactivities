53700e17d536d41b2191b25b6856428d05f75f76
PauleenSayat <163841476+PauleenSayat@users.noreply.github.com>
2024-08-30 14:26:55 +0800

    Add files via upload

---
+++ Exercise Sets/Exercise Set 01/Escseq.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        int value = 7;
+        int product;
+        for(int i = 1; i <= 5; i++){
+            
+            if (i%2==0){
+                product = i*value;
+                System.out.println("\t" + product);
+            } else{
+                product = i*value;
+                System.out.println(product);
+            }
+        }
+    }
+}
---
+++ Exercise Sets/Exercise Set 01/Tempconvert.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a Celsius value: ");
+        double Celsius = input.nextFloat();
+        double Fahrenheit = Celsius *(9/5) + 32;
+        
+        System.out.println(Celsius + " Celsius is " + Fahrenheit +" Fahrenheit.");
+        input.close();
+    }
+}
