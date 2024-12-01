636888094279e8ad3c2868e8ec9bf07d97e89e8a
PauleenSayat <163841476+PauleenSayat@users.noreply.github.com>
2024-12-01 22:49:27 +0800

    Add files via upload

---
+++ Final Project/Digital Banking Portal/Account.java
@@ -0,0 +1,40 @@
+package project;
+public class Account {
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Successfully deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Successfully withdrew: $" + amount);
+        } else {
+            System.out.println("Invalid or insufficient funds.");
+        }
+    }
+
+    public void displayBalance() {
+        System.out.println("Current balance: $" + balance);
+    }
+}
